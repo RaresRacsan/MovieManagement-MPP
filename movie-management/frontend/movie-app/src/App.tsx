2e31275f7b170e26ef0abb413ee6a0b8d00a276d
916-Racsan-Rares <rares.racsan@gmail.com>
2025-03-10 17:09:14 +0200

    initial hello world springboot + react

---
+++ movie-management/frontend/movie-app/src/App.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/hello")
+      .then((response) => response.text())
+      .then((data) => setMessage(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
